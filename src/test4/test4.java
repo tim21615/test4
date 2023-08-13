90a8a1d425718e97feba4708371bdfaa2e92ec40
李奇峰 <tim21615@gmail.com>
2023-08-13 11:04:52 +0800

    first commit

---
+++ src/test4/test4.java
@@ -0,0 +1,26 @@
+package test4;
+
+public class test4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		for(int ii=0;ii<3;ii++) 
+//		{
+//			
+//			int count =0;
+//			
+//			for(int jj=3;jj>0;jj--) 
+//			{
+//				if(ii==jj) 
+//				{
+//					++count;
+//					break;
+//				}
+//			}
+//			Systme.out.println(count);
+//			continue;
+//		}
+		System.out.println(123);
+	}
+
+}
